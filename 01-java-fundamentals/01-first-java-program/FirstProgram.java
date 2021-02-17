aaf39b7765775cc64fe563f8313b9a86ca00b0d1
brandigreene <72356558+brandigreene@users.noreply.github.com>
2021-02-17 15:54:42 -0600

    Add files via upload

---
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -0,0 +1,18 @@
+public class FirstProgram {
+    public static void main (String[] args) {
+        System.out.println("My name is Brandi Greene");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Conway, Arkansas");
+    }
+}
+
+public static void main FirstProgram {
+    System.out.println("There are items that need to be done")
+    System.out.println ("I am from Arkansas")
+    System.out.println ("My sister graduated from the University of Arkanas yesterday")
+    
+
+    
+
+
+}
